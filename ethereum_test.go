43e000e8e50cd965e77cd1860d15712b6aedcbc9
jinguihua <156711203@qq.com>
2025-09-30 11:05:49 +0800

    changed

--- ethereum_test.go
+++ ethereum_test.go
@@ -3,17 +3,28 @@ package mychain
 import (
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestEthChain_IsValidAddress(t *testing.T) {
-	b := ImpEth("AJES32DY7H7V4PVVPD7YYCJJKP84C37G1P").IsValidAddress("0x4d9cf1320060d198579220548877da4885e6497c")
+	b := ImpEth("").IsValidAddress("0x4d9cf1320060d198579220548877da4885e6497c")
 	t.Log("check result :", b)
 	t.Log(strings.ToLower("1CE24Ad9908A0964acC91b8EdbD104DD6F9FFAC4"))
 	t.Log(strings.ToLower("0017da119Ff092F6c3019F6490385E921067f657"))
 }
 
+func TestEthChain_GetNowBlockNum(t *testing.T) {
+	info, err := ImpEth("").GetNowBlockNum(time.Second * 10)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log("info is :", info.ToNumber())
+}
+
 func TestEthChain_GetETHTransactions(t *testing.T) {
-	v, err := ImpEth("AJES32DY7H7V4PVVPD7YYCJJKP84C37G1P").
+	v, err := ImpEth("").
 		GetETHTransactions("0x0011f4d21657905d3f90945db12b2516c075d2e0")
 	if err != nil {
 		t.Error(err)
